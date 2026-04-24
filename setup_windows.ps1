d2cee976d44eddc8e3b004c76c5d87de59bcd641
MATHEUS-DE-ANDRADE-LOURENCO <10419691@mackenzista.com.br>
2026-04-24 16:25:07 -0300

    Edição do setup

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -4,16 +4,6 @@ python -m venv venv
 Write-Host "Ativando ambiente e instalando dependências..."
 .\venv\Scripts\Activate.ps1
 python -m pip install --upgrade pip
-python -m pip install opencv-python ultralytics pandas numpy matplotlib pycocotools kagglehub
-
-Write-Host "Baixando COCO dataset..."
-Invoke-WebRequest -Uri "http://images.cocodataset.org/zips/val2017.zip" -OutFile "val2017.zip"
-Invoke-WebRequest -Uri "http://images.cocodataset.org/zips/test2017.zip" -OutFile "test2017.zip"
-Invoke-WebRequest -Uri "http://images.cocodataset.org/annotations/annotations_trainval2017.zip" -OutFile "annotations_trainval2017.zip"
-
-Write-Host "Descompactando arquivos..."
-Expand-Archive -Path "val2017.zip" -DestinationPath "." -Force
-Expand-Archive -Path "test2017.zip" -DestinationPath "." -Force
-Expand-Archive -Path "annotations_trainval2017.zip" -DestinationPath "." -Force
+python -m pip install opencv-python ultralytics pandas numpy matplotlib pycocotools kagglehub requests random time
 
 Write-Host "Setup concluído!"
