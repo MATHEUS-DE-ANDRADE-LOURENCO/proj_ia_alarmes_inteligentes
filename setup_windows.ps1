8914c40c54fd45e8a10d25de79fa6502c0379fd6
MATHEUS-DE-ANRADE-LOURENCO <10419691@mackenzista.com.br>
2026-04-27 12:58:16 -0300

    finalizado.

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -4,6 +4,6 @@ python -m venv venv
 Write-Host "Ativando ambiente e instalando dependências..."
 .\venv\Scripts\Activate.ps1
 python -m pip install --upgrade pip
-python -m pip install opencv-python ultralytics pandas numpy matplotlib pycocotools kagglehub requests random time
+python -m pip install opencv-python ultralytics pandas numpy matplotlib pycocotools kagglehub requests
 
 Write-Host "Setup concluído!"
